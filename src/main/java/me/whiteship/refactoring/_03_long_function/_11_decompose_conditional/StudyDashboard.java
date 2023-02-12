24ab9bdc46f06715309ce28ac096b59c6d8e5d35
rootsong8774 <95426072+rootsong8774@users.noreply.github.com>
2023-02-12 16:16:49 +0900

    리팩토링 11. 조건문 분해하기

--- src/main/java/me/whiteship/refactoring/_03_long_function/_11_decompose_conditional/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_11_decompose_conditional/StudyDashboard.java
@@ -35,22 +35,19 @@ public class StudyDashboard {
 
         for (int index = 1 ; index <= totalNumberOfEvents ; index++) {
             int eventId = index;
-            service.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        GHIssue issue = repository.getIssue(eventId);
-                        List<GHIssueComment> comments = issue.getComments();
+            service.execute(() -> {
+                try {
+                    GHIssue issue = repository.getIssue(eventId);
+                    List<GHIssueComment> comments = issue.getComments();
 
-                        for (GHIssueComment comment : comments) {
-                            Participant participant = findParticipant(comment.getUserName(), participants);
-                            participant.setHomeworkDone(eventId);
-                        }
-
-                        latch.countDown();
-                    } catch (IOException e) {
-                        throw new IllegalArgumentException(e);
+                    for (GHIssueComment comment : comments) {
+                        Participant participant = findParticipant(comment.getUserName(), participants);
+                        participant.setHomeworkDone(eventId);
                     }
+
+                    latch.countDown();
+                } catch (IOException e) {
+                    throw new IllegalArgumentException(e);
                 }
             });
         }
@@ -62,15 +59,28 @@ public class StudyDashboard {
     }
 
     private Participant findParticipant(String username, List<Participant> participants) {
+        return isNewParticipant(username, participants) ? createNewParticipant(username, participants) : findExistingParticipant(username, participants);
+    }
+    
+    private static Participant findExistingParticipant(String username, List<Participant> participants) {
+        return participants.stream()
+                           .filter(p -> p.username()
+                                         .equals(username))
+                           .findFirst()
+                           .orElseThrow();
+    }
+    
+    private static Participant createNewParticipant(String username, List<Participant> participants) {
         Participant participant;
-        if (participants.stream().noneMatch(p -> p.username().equals(username))) {
-            participant = new Participant(username);
-            participants.add(participant);
-        } else {
-            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
-        }
-
+        participant = new Participant(username);
+        participants.add(participant);
         return participant;
     }
-
+    
+    private static boolean isNewParticipant(String username, List<Participant> participants) {
+        return participants.stream()
+                           .noneMatch(p -> p.username()
+                                            .equals(username));
+    }
+    
 }
